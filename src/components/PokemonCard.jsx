f772d3d7ee1a13c4822d666a9bd19b96f1beee3c
shashwat2048 <shinghalshashwat@gmail.com>
2025-04-26 09:00:30 +0530

    feat: Update PokemonCard component to display additional sprite image

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,7 +2,7 @@ export default function PokemonCard({pokemon}){
     return (
         <div className="border-2 border-black">
             <h2>{pokemon.name}</h2>
-            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            <img src={pokemon.sprites.other.showdown.front_default} alt={pokemon.name} />
             <p>Height: {pokemon.height}</p>
             <p>Weight: {pokemon.weight}</p>
             <p>Base Experience: {pokemon.base_experience}</p>
